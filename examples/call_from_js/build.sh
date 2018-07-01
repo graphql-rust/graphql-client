ba63b4e2ddc06f24c335fa979e08d5f8b86ca5c4
Tom Houlé <tom@tomhoule.com>
2018-07-01 21:13:49 +0200

    Fix the wasm example
    It requires nightly (because of wasm-bindgen). Also other small things.

--- examples/call_from_js/build.sh
+++ examples/call_from_js/build.sh
@@ -1,6 +1,6 @@
 set -ex
 
-cargo +nightly build --target wasm32-unknown-unknown --release
+cargo +nightly build --target wasm32-unknown-unknown
 
 wasm-bindgen \
   ../../target/wasm32-unknown-unknown/debug/call_from_js.wasm --out-dir .
