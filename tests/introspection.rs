f48f3116225eae5d4a7a90fd6575af5da3413342
Tom Houlé <tom@tomhoule.com>
2018-08-22 19:29:29 +0200

    Test that leading underscores are preserved

--- tests/introspection.rs
+++ tests/introspection.rs
@@ -3,11 +3,13 @@ extern crate graphql_client;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
+extern crate serde_json;
 
 #[derive(GraphQLQuery)]
 #[graphql(
     query_path = "tests/introspection/introspection_query.graphql",
     schema_path = "tests/introspection/introspection_schema.graphql",
+    response_derives = "Debug,PartialEq"
 )]
 #[allow(dead_code)]
 struct IntrospectionQuery;
@@ -15,4 +17,15 @@ struct IntrospectionQuery;
 #[test]
 fn introspection_schema() {
     ()
+}
+
+const INTROSPECTION_RESPONSE: &'static str =
+    include_str!("./introspection/introspection_response.json");
+
+#[test]
+fn leading_underscores_are_preserved() {
+    let deserialized: graphql_client::GraphQLResponse<introspection_query::ResponseData> =
+        serde_json::from_str(INTROSPECTION_RESPONSE).unwrap();
+    assert!(deserialized.data.is_some());
+    assert!(deserialized.data.unwrap().schema.is_some());
 }
