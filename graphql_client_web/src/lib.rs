c183af0453c9a409ff3bd6048e1bcde3b50fc573
Tom Houlé <tom@tomhoule.com>
2018-10-17 19:33:32 +0200

    Add a graphql_client_web crate for browser usage

---
+++ graphql_client_web/src/lib.rs
@@ -0,0 +1,146 @@
+//! Use graphql_client inside browsers with [wasm-bindgen].
+//!
+//! This crate reexports all you need from graphql-client, so you do not need any other explicit dependencies.
+
+// #![deny(warnings)]
+#![deny(missing_docs)]
+
+#[macro_use]
+pub extern crate graphql_client;
+#[macro_use]
+extern crate wasm_bindgen;
+
+use failure::*;
+use futures::{Future, IntoFuture};
+pub use graphql_client::GraphQLQuery;
+use log::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen::JsValue;
+use wasm_bindgen_futures::JsFuture;
+
+/// The main interface to the library.
+///
+/// The workflow is the following:
+///
+/// - create a client
+/// - (optionally) configure it
+/// - use it to perform queries with the [call] method
+pub struct Client {
+    endpoint: String,
+}
+
+/// All the ways a request can go wrong.
+///
+/// not exhaustive
+#[derive(Debug, Fail)]
+pub enum ClientError {
+    /// The body couldn't be built
+    #[fail(display = "Request body is not a valid string")]
+    Body,
+    /// An error caused by window.fetch
+    #[fail(display = "Network error")]
+    Network,
+    /// Error in a dynamic JS cast that should have worked
+    #[fail(display = "JS casting error")]
+    Cast,
+    /// No window object could be retrieved
+    #[fail(
+        display = "No Window object available - the client works only in a browser (non-worker) context"
+    )]
+    NoWindow,
+    /// Response shape does not match the generated code
+    #[fail(display = "Response shape error",)]
+    ResponseShape,
+    /// Response could not be converted to text
+    #[fail(display = "Response conversion to text failed (Response.text threw)")]
+    ResponseText,
+    /// Exception thrown when building the request
+    #[fail(display = "Error building the request",)]
+    RequestError,
+    /// Other JS exception
+    #[fail(display = "Unexpected JS exception")]
+    JsException,
+}
+
+impl Client {
+    /// Initialize a client. The `endpoint` parameter is the URI of the GraphQL API.
+    pub fn new<Endpoint>(endpoint: Endpoint) -> Client
+    where
+        Endpoint: Into<String>,
+    {
+        Client {
+            endpoint: endpoint.into(),
+        }
+    }
+
+    /// Perform a query.
+    pub fn call<Q: GraphQLQuery + 'static>(
+        &self,
+        _query: Q,
+        variables: Q::Variables,
+    ) -> impl Future<Item = graphql_client::Response<Q::ResponseData>, Error = ClientError> + 'static
+    {
+        // this can be removed when we convert to async/await
+        let endpoint = self.endpoint.clone();
+
+        web_sys::window()
+            .ok_or_else(|| ClientError::NoWindow)
+            .into_future()
+            .and_then(move |window| {
+                serde_json::to_string(&Q::build_query(variables))
+                    .map_err(|_| ClientError::Body)
+                    .map(move |body| (window, body))
+            }).and_then(move |(window, body)| {
+                let mut request_init = web_sys::RequestInit::new();
+                request_init
+                    .method("POST")
+                    .body(Some(&JsValue::from_str(&body)));
+
+                web_sys::Request::new_with_str_and_init(&endpoint, &request_init)
+                    .map_err(|_| ClientError::JsException)
+                    .map(|request| (window, request))
+                // "Request constructor threw");
+            }).and_then(move |(window, request)| {
+                let request: Result<web_sys::Request, _> = request
+                    .headers()
+                    .set("Content-Type", "application/json")
+                    .map_err(|_| ClientError::RequestError)
+                    .map(|_| request);
+
+                let request: Result<web_sys::Request, _> = request.and_then(|req| {
+                    req.headers()
+                        .set("Accept", "application/json")
+                        .map_err(|_| ClientError::RequestError)
+                        .map(|_| req)
+                });
+
+                request.map(move |request| (window, request))
+            }).and_then(move |(window, request)| {
+                JsFuture::from(window.fetch_with_request(&request))
+                    .map_err(|_| ClientError::Network)
+            }).and_then(move |res| {
+                debug!("response: {:?}", res);
+                res.dyn_into::<web_sys::Response>()
+                    .map_err(|_| ClientError::Cast)
+            }).and_then(move |cast_response| {
+                cast_response.text().map_err(|_| ClientError::ResponseText)
+            }).and_then(move |text_promise| {
+                JsFuture::from(text_promise).map_err(|_| ClientError::ResponseText)
+            }).and_then(|text| {
+                let response_text = text.as_string().unwrap_or_else(|| String::new());
+                debug!("response text as string: {:?}", response_text);
+                serde_json::from_str(&response_text).map_err(|_| ClientError::ResponseShape)
+            })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn client_new() {
+        Client::new("https://example.com/graphql");
+        Client::new("/graphql");
+    }
+}
---
+++ graphql_client_web/tests/web.rs
@@ -0,0 +1,78 @@
+#[macro_use]
+extern crate graphql_client_web;
+
+#[macro_use]
+extern crate serde_derive;
+
+use futures::Future;
+use graphql_client_web::Client;
+use wasm_bindgen::JsValue;
+use wasm_bindgen_test::*;
+
+use std::panic;
+
+use wasm_bindgen_test::wasm_bindgen_test_configure;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+fn build_client() {
+    Client::new("https://example.com/graphql");
+    Client::new("/graphql");
+}
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "tests/countries_schema.json",
+    query_path = "tests/Germany.graphql"
+)]
+struct Germany;
+
+#[wasm_bindgen_test(async)]
+fn test_germany() -> impl Future<Item = (), Error = JsValue> {
+    Client::new("https://countries.trevorblades.com/")
+        .call(Germany, germany::Variables)
+        .map(|response| {
+            let continent_name = response
+                .data
+                .expect("response data is not null")
+                .country
+                .expect("country is not null")
+                .continent
+                .expect("continent is not null")
+                .name
+                .expect("germany is on a continent");
+
+            assert_eq!(continent_name, "Europe");
+        }).map_err(|_| JsValue::NULL)
+}
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "tests/countries_schema.json",
+    query_path = "tests/Germany.graphql"
+)]
+struct Country;
+
+#[wasm_bindgen_test(async)]
+fn test_country() -> impl Future<Item = (), Error = JsValue> {
+    Client::new("https://countries.trevorblades.com/")
+        .call(
+            Country,
+            country::Variables {
+                country_code: "CN".to_owned(),
+            },
+        ).map(|response| {
+            let continent_name = response
+                .data
+                .expect("response data is not null")
+                .country
+                .expect("country is not null")
+                .continent
+                .expect("continent is not null")
+                .name
+                .expect("country is on a continent");
+
+            assert_eq!(continent_name, "Asia");
+        }).map_err(|_| JsValue::NULL)
+}
