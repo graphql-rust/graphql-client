62ac7e932407f490fbae0ab56a0e5c3599767bc5
Tom Houlé <tom@tomhoule.com>
2018-12-26 17:39:49 +0100

    Use the firefox addon in CI
    https://docs.travis-ci.com/user/gui-and-headless-browsers/

--- scripts/install_geckodriver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script is meant for CI only.
-
-set -ex
-
-wget https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
-tar xzf geckodriver-v0.23.0-linux64.tar.gz
