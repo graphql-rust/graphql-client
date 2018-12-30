9a0acd46d8108d7f48de1c006153e09f115bd58c
Tom Houlé <tom@tomhoule.com>
2018-12-30 13:49:57 +0100

    Move examples directory back to the repo root

--- graphql_client/examples/web/web_bg.d.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-export const memory: WebAssembly.Memory;
-export function __wbindgen_malloc(a: number): number;
-export function run(): void;
-export const __wbg_function_table: WebAssembly.Table;
