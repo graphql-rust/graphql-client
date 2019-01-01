03d8eca196de656595086f169b47a9dfd5d600e6
Tom Houlé <tom@tomhoule.com>
2019-01-01 12:30:29 +0100

    Fix fragments on unions

--- graphql_client/tests/union_query.rs
+++ graphql_client/tests/union_query.rs
@@ -15,6 +15,14 @@ const RESPONSE: &'static str = include_str!("unions/union_query_response.json");
 )]
 pub struct UnionQuery;
 
+#[derive(GraphQLQuery)]
+#[graphql(
+    query_path = "tests/unions/union_query.graphql",
+    schema_path = "tests/unions/union_schema.graphql",
+    response_derives = "PartialEq, Debug"
+)]
+pub struct FragmentOnUnion;
+
 #[test]
 fn union_query_deserialization() {
     let response_data: union_query::ResponseData = serde_json::from_str(RESPONSE).unwrap();
@@ -40,4 +48,29 @@ fn union_query_deserialization() {
     assert_eq!(response_data, expected);
 
     assert_eq!(response_data.names.map(|names| names.len()), Some(4));
+}
+
+#[test]
+fn fragment_on_union() {
+    let response_data: fragment_on_union::ResponseData = serde_json::from_str(RESPONSE).unwrap();
+
+    let expected = fragment_on_union::ResponseData {
+        names: Some(vec![
+            fragment_on_union::MyQueryNames::Person(fragment_on_union::MyQueryNamesOnPerson {
+                first_name: "Audrey".to_string(),
+                last_name: Some("Lorde".to_string()),
+            }),
+            fragment_on_union::MyQueryNames::Dog(fragment_on_union::MyQueryNamesOnDog {
+                name: "Laïka".to_string(),
+            }),
+            fragment_on_union::MyQueryNames::Organization(fragment_on_union::MyQueryNamesOnOrganization {
+                title: "Mozilla".to_string(),
+            }),
+            fragment_on_union::MyQueryNames::Dog(fragment_on_union::MyQueryNamesOnDog {
+                name: "Norbert".to_string(),
+            }),
+        ]),
+    };
+
+    assert_eq!(response_data, expected);
 }
