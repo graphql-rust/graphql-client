4657dcbc5c263dbf6ba07488ede2a437bbd0289c
Tom Houlé <tom@tomhoule.com>
2020-03-20 23:05:01 +0100

    Polish json conversion

--- graphql_client_codegen/src/schema/json_conversion.rs
+++ graphql_client_codegen/src/schema/json_conversion.rs
@@ -3,223 +3,89 @@ use graphql_introspection_query::introspection_response::{
     FullType, IntrospectionResponse, Schema as JsonSchema, TypeRef, __TypeKind,
 };
 
-pub(super) fn build_schema(src: IntrospectionResponse) -> super::Schema {
-    let converter = JsonSchemaConverter {
-        src: src.into_schema().schema.expect("could not find schema"),
-        schema: Schema::new(),
-    };
+pub(super) fn build_schema(src: IntrospectionResponse) -> Schema {
+    let mut src = src.into_schema().schema.expect("could not find schema");
+    let mut schema = Schema::new();
+    build_names_map(&mut src, &mut schema);
+    convert(&mut src, &mut schema);
 
-    converter.convert()
+    schema
 }
 
-struct JsonSchemaConverter {
-    src: JsonSchema,
-    schema: Schema,
+fn build_names_map(src: &mut JsonSchema, schema: &mut Schema) {
+    let mut names = &mut schema.names;
+    names.reserve(types_mut(src).count());
+
+    unions_mut(src)
+        .map(|u| u.name.as_ref().expect("union name"))
+        .enumerate()
+        .for_each(|(idx, name)| {
+            names.insert(name.clone(), TypeId::union(idx));
+        });
+
+    interfaces_mut(src)
+        .map(|iface| iface.name.as_ref().expect("interface name"))
+        .enumerate()
+        .for_each(|(idx, name)| {
+            names.insert(name.clone(), TypeId::interface(idx));
+        });
+
+    objects_mut(src)
+        .map(|obj| obj.name.as_ref().expect("object name"))
+        .enumerate()
+        .for_each(|(idx, name)| {
+            names.insert(name.clone(), TypeId::object(idx));
+        });
+
+    inputs_mut(src)
+        .map(|obj| obj.name.as_ref().expect("input name"))
+        .enumerate()
+        .for_each(|(idx, name)| {
+            names.insert(name.clone(), TypeId::input(idx));
+        });
 }
 
-impl JsonSchemaConverter {
-    fn build_names_map(&mut self) {
-        self.schema.names.reserve(types_mut(&mut self.src).count());
-        let names = &mut self.schema.names;
-
-        unions_mut(&mut self.src)
-            .map(|u| u.name.as_ref().expect("union name"))
-            .enumerate()
-            .for_each(|(idx, name)| {
-                names.insert(name.clone(), TypeId::union(idx));
-            });
-
-        interfaces_mut(&mut self.src)
-            .map(|iface| iface.name.as_ref().expect("interface name"))
-            .enumerate()
-            .for_each(|(idx, name)| {
-                names.insert(name.clone(), TypeId::interface(idx));
-            });
-
-        objects_mut(&mut self.src)
-            .map(|obj| obj.name.as_ref().expect("object name"))
-            .enumerate()
-            .for_each(|(idx, name)| {
-                names.insert(name.clone(), TypeId::object(idx));
-            });
-
-        inputs_mut(&mut self.src)
-            .map(|obj| obj.name.as_ref().expect("input name"))
-            .enumerate()
-            .for_each(|(idx, name)| {
-                names.insert(name.clone(), TypeId::input(idx));
-            });
+fn convert(src: &mut JsonSchema, schema: &mut Schema) {
+    for scalar in scalars_mut(src) {
+        ingest_scalar(schema, scalar);
     }
 
-    fn convert(mut self) -> Schema {
-        self.build_names_map();
-        let JsonSchemaConverter {
-            mut src,
-            mut schema,
-        } = self;
+    for enm in enums_mut(src) {
+        ingest_enum(schema, enm)
+    }
 
-        for scalar in scalars_mut(&mut src) {
-            ingest_scalar(&mut schema, scalar);
-        }
+    for interface in interfaces_mut(src) {
+        ingest_interface(schema, interface);
+    }
 
-        for enm in enums_mut(&mut src) {
-            ingest_enum(&mut schema, enm)
-        }
+    for object in objects_mut(src) {
+        ingest_object(schema, object);
+    }
 
-        for interface in interfaces_mut(&mut src) {
-            ingest_interface(&mut schema, interface);
-        }
+    for unn in unions_mut(src) {
+        ingest_union(schema, unn)
+    }
 
-        for object in objects_mut(&mut src) {
-            ingest_object(&mut schema, object);
-        }
-
-        // for ty in src
-        //     .types
-        //     .as_ref()
-        //     .expect("types in schema")
-        //     .iter()
-        //     .filter_map(|t| t.as_ref().map(|t| &t.full_type))
-        // {
-        //     let name: &str = ty
-        //         .name
-        //         .as_ref()
-        //         .map(String::as_str)
-        //         .expect("type definition name");
-
-        //     match ty.kind {
-        //         Some(__TypeKind::ENUM) => {
-        //             // let variants: Vec<EnumVariant<'_>> = ty
-        //             //     .enum_values
-        //             //     .as_ref()
-        //             //     .expect("enum variants")
-        //             //     .iter()
-        //             //     .map(|t| {
-        //             //         t.as_ref().map(|t| EnumVariant {
-        //             //             description: t.description.as_ref().map(String::as_str),
-        //             //             name: t
-        //             //                 .name
-        //             //                 .as_ref()
-        //             //                 .map(String::as_str)
-        //             //                 .expect("enum variant name"),
-        //             //         })
-        //             //     })
-        //             //     .filter_map(|t| t)
-        //             //     .collect();
-        //             // let enm = GqlEnum {
-        //             //     name,
-        //             //     description: ty.description.as_ref().map(String::as_str),
-        //             //     variants,
-        //             //     is_required: false.into(),
-        //             // };
-        //             // schema.enums.insert(name, enm);
-        //         }
-        //         Some(__TypeKind::SCALAR) => {
-        //             // if DEFAULT_SCALARS.iter().find(|s| s == &&name).is_none() {
-        //             //     schema.scalars.insert(
-        //             //         name,
-        //             //         Scalar {
-        //             //             name,
-        //             //             description: ty.description.as_ref().map(String::as_str),
-        //             //             is_required: false.into(),
-        //             //         },
-        //             //     );
-        //             // }
-        //         }
-        //         Some(__TypeKind::UNION) => {
-        //             // let variants: BTreeSet<&str> = ty
-        //             //     .possible_types
-        //             //     .as_ref()
-        //             //     .unwrap()
-        //             //     .iter()
-        //             //     .filter_map(|t| {
-        //             //         t.as_ref()
-        //             //             .and_then(|t| t.type_ref.name.as_ref().map(String::as_str))
-        //             //     })
-        //             //     .collect();
-        //             // schema.unions.insert(
-        //             //     name,
-        //             //     GqlUnion {
-        //             //         name: ty.name.as_ref().map(String::as_str).expect("unnamed union"),
-        //             //         description: ty.description.as_ref().map(String::as_str),
-        //             //         variants,
-        //             //         is_required: false.into(),
-        //             //     },
-        //             // );
-        //         }
-        //         Some(__TypeKind::OBJECT) => {
-        //             // for implementing in ty
-        //             //     .interfaces
-        //             //     .as_ref()
-        //             //     .map(Vec::as_slice)
-        //             //     .unwrap_or_else(|| &[])
-        //             //     .iter()
-        //             //     .filter_map(Option::as_ref)
-        //             //     .map(|t| &t.type_ref.name)
-        //             // {
-        //             //     interface_implementations
-        //             //         .entry(
-        //             //             implementing
-        //             //                 .as_ref()
-        //             //                 .map(String::as_str)
-        //             //                 .expect("interface name"),
-        //             //         )
-        //             //         .and_modify(|objects| objects.push(name))
-        //             //         .or_insert_with(|| vec![name]);
-        //             // }
-
-        //             // schema
-        //             //     .objects
-        //             //     .insert(name, GqlObject::from_introspected_schema_json(ty));
-        //         }
-        //         Some(__TypeKind::INTERFACE) => {
-        //             // let mut iface =
-        //             //     GqlInterface::new(name, ty.description.as_ref().map(String::as_str));
-        //             // iface.fields.extend(
-        //             //     ty.fields
-        //             //         .as_ref()
-        //             //         .expect("interface fields")
-        //             //         .iter()
-        //             //         .filter_map(Option::as_ref)
-        //             //         .map(|f| GqlObjectField {
-        //             //             description: f.description.as_ref().map(String::as_str),
-        //             //             name: f.name.as_ref().expect("field name").as_str(),
-        //             //             type_: FieldType::from(f.type_.as_ref().expect("field type")),
-        //             //             deprecation: DeprecationStatus::Current,
-        //             //         }),
-        //             // );
-        //             // schema.interfaces.insert(name, iface);
-        //         }
-        //         Some(__TypeKind::INPUT_OBJECT) => {
-        //             //     schema.inputs.insert(name, GqlInput::from(ty));
-        //         }
-        //         _ => unimplemented!("unimplemented definition"),
-        //     }
-        // }
-
-        // Define the root operations.
-        {
-            schema.query_type = src
-                .query_type
-                .as_mut()
-                .and_then(|n| n.name.as_mut())
-                .and_then(|n| schema.names.get(n))
-                .and_then(|id| id.as_object_id());
-            schema.mutation_type = src
-                .mutation_type
-                .as_mut()
-                .and_then(|n| n.name.as_mut())
-                .and_then(|n| schema.names.get(n))
-                .and_then(|id| id.as_object_id());
-            schema.subscription_type = src
-                .mutation_type
-                .as_mut()
-                .and_then(|n| n.name.as_mut())
-                .and_then(|n| schema.names.get(n))
-                .and_then(|id| id.as_object_id());
-        }
-
-        schema
+    // Define the root operations.
+    {
+        schema.query_type = src
+            .query_type
+            .as_mut()
+            .and_then(|n| n.name.as_mut())
+            .and_then(|n| schema.names.get(n))
+            .and_then(|id| id.as_object_id());
+        schema.mutation_type = src
+            .mutation_type
+            .as_mut()
+            .and_then(|n| n.name.as_mut())
+            .and_then(|n| schema.names.get(n))
+            .and_then(|id| id.as_object_id());
+        schema.subscription_type = src
+            .mutation_type
+            .as_mut()
+            .and_then(|n| n.name.as_mut())
+            .and_then(|n| schema.names.get(n))
+            .and_then(|id| id.as_object_id());
     }
 }
 
