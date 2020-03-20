3c863f005a9406c127dbc47eee143c19483be740
Tom Houlé <tom@tomhoule.com>
2020-03-20 23:05:01 +0100

    Polish json conversion

--- graphql_client_codegen/src/field_type.rs
+++ graphql_client_codegen/src/field_type.rs
@@ -12,21 +12,10 @@ impl GraphqlTypeQualifier {
     }
 }
 
-pub(crate) fn graphql_parser_depth(schema_type: &graphql_parser::schema::Type) -> usize {
+pub fn graphql_parser_depth(schema_type: &graphql_parser::schema::Type) -> usize {
     match schema_type {
         graphql_parser::schema::Type::ListType(inner) => 1 + graphql_parser_depth(inner),
         graphql_parser::schema::Type::NonNullType(inner) => 1 + graphql_parser_depth(inner),
         graphql_parser::schema::Type::NamedType(_) => 0,
-    }
-}
-
-fn json_type_qualifiers_depth(typeref: &introspection_response::TypeRef) -> usize {
-    use graphql_introspection_query::introspection_response::*;
-
-    match (typeref.kind.as_ref(), typeref.of_type.as_ref()) {
-        (Some(__TypeKind::NON_NULL), Some(inner)) => 1 + json_type_qualifiers_depth(inner),
-        (Some(__TypeKind::LIST), Some(inner)) => 1 + json_type_qualifiers_depth(inner),
-        (Some(_), None) => 0,
-        _ => panic!("Non-convertible type in JSON schema: {:?}", typeref),
     }
 }
