9bc7f50506c108707778883de9b82ade2c3fa433
Domenico Visconti <domenico.visconti@prima.it>
2021-07-04 17:43:11 +0200

    Revert "create Unknow variant for fragment as default behaviour"
    This reverts commit 20c8e6dd875f1d53868cd181285499c3e6637682.

--- graphql_client_codegen/src/tests/mod.rs
+++ graphql_client_codegen/src/tests/mod.rs
@@ -46,6 +46,45 @@ fn fragments_other_variant_should_generate_unknown_other_variant() {
         .expect("Parse foobars schema");
     let schema = Schema::from(schema);
 
+    let mut options = GraphQLClientCodegenOptions::new(CodegenMode::Cli);
+
+    options.set_fragments_other_variant(true);
+    let query = crate::query::resolve(&schema, &query).unwrap();
+
+    for (_id, operation) in query.operations() {
+        let generated_tokens = generated_module::GeneratedModule {
+            query_string,
+            schema: &schema,
+            operation: &operation.name,
+            resolved_query: &query,
+            options: &options,
+        }
+        .to_token_stream()
+        .expect("Generate foobars module");
+        let generated_code = generated_tokens.to_string();
+
+        let r: syn::parse::Result<proc_macro2::TokenStream> = syn::parse2(generated_tokens);
+        match r {
+            Ok(_) => {
+                // Rust keywords should be escaped / renamed now
+                assert!(generated_code.contains("# [serde (other)] Unknown"));
+                assert!(generated_code.contains("Unknown"));
+            }
+            Err(e) => {
+                panic!("Error: {}\n Generated content: {}\n", e, &generated_code);
+            }
+        };
+    }
+}
+
+#[test]
+fn fragments_other_variant_false_should_not_generate_unknown_other_variant() {
+    let query_string = include_str!("foobars_query.graphql");
+    let query = graphql_parser::parse_query(query_string).expect("Parse foobars query");
+    let schema = graphql_parser::parse_schema(include_str!("foobars_schema.graphql"))
+        .expect("Parse foobars schema");
+    let schema = Schema::from(schema);
+
     let options = GraphQLClientCodegenOptions::new(CodegenMode::Cli);
 
     let query = crate::query::resolve(&schema, &query).unwrap();
@@ -66,7 +105,8 @@ fn fragments_other_variant_should_generate_unknown_other_variant() {
         match r {
             Ok(_) => {
                 // Rust keywords should be escaped / renamed now
-                assert!(generated_code.contains("# [serde (other)] Unknown"));
+                assert!(!generated_code.contains("# [serde (other)] Unknown"));
+                assert!(!generated_code.contains("Unknown"));
             }
             Err(e) => {
                 panic!("Error: {}\n Generated content: {}\n", e, &generated_code);
