cf1249803c08cd4a331c73fdebe5c85ccc8d0341
daniel.eades <daniel.eades@seebyte.com>
2025-01-31 11:24:18 +0000

    use 'assert' macro (clippy::manual_assert)

--- graphql_client_cli/src/generate.rs
+++ graphql_client_cli/src/generate.rs
@@ -138,12 +138,11 @@ fn format(code: &str) -> CliResult<String> {
 
     let output = child.wait_with_output()?;
 
-    if !output.status.success() {
-        panic!(
-            "rustfmt error\n\n{}",
-            String::from_utf8_lossy(&output.stderr)
-        );
-    }
+    assert!(
+        output.status.success(),
+        "rustfmt error\n\n{}",
+        String::from_utf8_lossy(&output.stderr)
+    );
 
     Ok(String::from_utf8(output.stdout)?)
 }
