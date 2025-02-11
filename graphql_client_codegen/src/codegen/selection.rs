cba75ea9957354a4a9eb6152952bcf44f81fccb3
daniel.eades <daniel.eades@seebyte.com>
2025-02-11 16:01:36 +0000

    fix formatting

--- graphql_client_codegen/src/codegen/selection.rs
+++ graphql_client_codegen/src/codegen/selection.rs
@@ -406,11 +406,15 @@ impl ExpandedField<'_> {
         };
 
         let is_id = self.field_type == "ID";
-        let is_required = self.field_type_qualifiers.contains(&GraphqlTypeQualifier::Required);
+        let is_required = self
+            .field_type_qualifiers
+            .contains(&GraphqlTypeQualifier::Required);
         let id_deserialize_with = if is_id && is_required {
             Some(quote!(#[serde(deserialize_with = "graphql_client::serde_with::deserialize_id")]))
         } else if is_id {
-            Some(quote!(#[serde(deserialize_with = "graphql_client::serde_with::deserialize_option_id")]))
+            Some(
+                quote!(#[serde(deserialize_with = "graphql_client::serde_with::deserialize_option_id")]),
+            )
         } else {
             None
         };
